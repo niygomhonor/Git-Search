126fee35ba51b310938fe017f1453f78edb23e95
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-09-01 02:57:06 +0200

    Passing parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { SearchComponent } from './search-form/search/search.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'profiles/:name', component:ProfileComponent},
+  {path:"repository/:reposit" ,component:RepositoryComponent},
+  {path:'',redirectTo:"/profiles",pathMatch:"full"},
+  {path:"**",component:ProfileComponent},
+  {path:'search', component:SearchComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
